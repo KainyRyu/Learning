3637e656c42ae51e945b90fa36067ad845378d8f
KainyRyu <KainyRyu@users.noreply.github.com>
2019-12-18 12:38:23 +0000

    Adding summary files
    for html, css and js (+jQuery)

---
+++ Projects/recycle/map.js
@@ -0,0 +1,5 @@
+window.addEventListener("load",function(){
+    var svgObject = document.getElementById('svg-object').contentDocument;
+    var svg = svgObject.getElementById('external-1');
+    this.console.log(svg);
+})
